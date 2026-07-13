05abb8ef967bd4957c1273c1d8490c4a5f6c7c8d
Carlos Daniel <carlosdanieldiasmendes@gmail.com>
2026-07-13 10:47:01 -0300

    fix:routes para lazyLoad

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,29 +1,30 @@
 import { Routes } from '@angular/router';
-import { WorkPage } from './pages/work-page/work-page';
-import { SeriePage } from './pages/serie-page/serie-page';
-import { LoginPage } from './pages/login-page/login-page';
-import { Wrapper } from './router/wrapper/wrapper';
-import { HomePage } from './pages/home/home-page';
 import { authGuard } from './guards/auth-guard';
 import { guestGuard } from './guards/guest-guard';
-import { ManagementPage } from './pages/management-page/management-page';
 import { adminGuard } from './guards/admin-guard';
-import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Wrapper,
+    loadComponent: () => import('./router/wrapper/wrapper').then((m) => m.Wrapper),
     canActivate: [authGuard],
     children: [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomePage },
-      { path: 'series', component: SeriePage },
-      { path: 'works', component: WorkPage },
-      { path: 'management', component: ManagementPage, canActivate: [adminGuard] },
+      { path: 'home', loadComponent: () => import('./pages/home/home-page').then((m) => m.HomePage) },
+      { path: 'series', loadComponent: () => import('./pages/serie-page/serie-page').then((m) => m.SeriePage) },
+      { path: 'works', loadComponent: () => import('./pages/work-page/work-page').then((m) => m.WorkPage) },
+      {
+        path: 'management',
+        loadComponent: () => import('./pages/management-page/management-page').then((m) => m.ManagementPage),
+        canActivate: [adminGuard],
+      },
     ],
   },
-  { path: 'auth/login', component: LoginPage, canActivate: [guestGuard] },
-  { path: 'not-found', component: NotFound },
-  { path: '**', component: NotFound },
+  {
+    path: 'auth/login',
+    loadComponent: () => import('./pages/login-page/login-page').then((m) => m.LoginPage),
+    canActivate: [guestGuard],
+  },
+  { path: 'not-found', loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound) },
+  { path: '**', loadComponent: () => import('./pages/not-found/not-found').then((m) => m.NotFound) },
 ];
